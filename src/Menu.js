e0a1facdb260dc79c6f03017b9a0167c0bcab1df
oluwajueda <oluwajuedaloade@gmail.com>
2022-07-17 16:36:10 -0700

    changes made

--- src/Menu.js
+++ src/Menu.js
@@ -21,7 +21,7 @@ const Menu = () => {
             </div>
             <div>
             <h4 className='product-details'>see details...</h4>
-            <img className='details-addto' src={addto} alt='name'/>
+            <img className='addto-btn' src={addto} alt='name'/>
             </div>
             </div>
             </div>
